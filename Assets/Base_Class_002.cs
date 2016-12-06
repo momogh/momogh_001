940ba9515dc55b01ac1d18ff524d420628bb90ce
momogh <enrico.montorsi.11@gmail.com>
2016-12-06 16:19:16 +0100

    Aggiunte classi 001 e 002 + Scena
    06-12-2016
    - aggiunti file "Base_Class_001.cs" e "Base_Class_002.cs".
    - Aggiunto file "Scene_001"

---
+++ Assets/Base_Class_002.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Base_Class_002 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
